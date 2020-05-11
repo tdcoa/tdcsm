1df292e5147bf65a082e36ed9877e16efa5ca0ec
desainn <desainn09@gmail.com>
2020-05-11 09:56:00 -0400

    removing unneeded step from run.py

--- run.py
+++ run.py
@@ -5,35 +5,30 @@ coa = tdcoa(r'C:\Users\nd186026\Documents\tdcsm_demo')
 options = """
 Select a step:
 
-1) Setup
-2) Download Files
-3) Prepare SQL
-4) Execute Run
-5) Upload to Transcend
+1) Download Files
+2) Prepare SQL
+3) Execute Run
+4) Upload to Transcend
 """
 
 while True:
     x = input(options)
 
     try:
-        # 1) SETUP
+        # 1) DOWNLOAD FILES
         if int(x) == 1:
-            pass
-
-        # 2) DOWNLOAD FILES
-        elif int(x) == 2:
             coa.download_files()
 
-        # 3) PREPARE SQL
-        elif int(x) == 3:
+        # 2) PREPARE SQL
+        elif int(x) == 2:
             coa.prepare_sql()
 
-        # 4) EXECUTE SQL
-        elif int(x) == 4:
+        # 3) EXECUTE SQL
+        elif int(x) == 3:
             coa.execute_run()
 
-        # 5) UPLOAD TO TRANSCEND
-        elif int(x) == 5:
+        # 4) UPLOAD TO TRANSCEND
+        elif int(x) == 4:
             coa.upload_to_transcend()
             break
 
