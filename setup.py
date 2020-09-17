902f0d20f7dac7574f7676059a5bbb93e363a402
Paresh Adhia <paresh_adhia@yahoo.com>
2020-09-17 14:44:31 -0400

    intaller now checks for locations to create shortcuts;removed code that tries to fix -- comments

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tdcsm",
-    version = "0.4.0.6",
+    version = "0.4.0.7",
     author="Stephen Hilton",
     author_email="Stephen@FamilyHilton.com",
     description="Teradata tools for CSMs",
